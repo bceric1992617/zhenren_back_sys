0f8d54de0bac0c00dcfc711dbbff39f4c9d16777
bceric1992617@gmail.com <bceric1992617@gmail.com>
2022-05-08 14:06:15 +0700

    first commit

---
+++ src/page/business/index.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+    <el-aside>
+        <SideMenu :items="items"></SideMenu>
+    </el-aside>
+    <el-main>
+        <router-view></router-view>
+    </el-main>
+</div>
+</template>
+
+<script>
+import SideMenu from '@/components/sidemenu/SideMenu';
+export default {
+    components: {
+        SideMenu
+    },
+    data() {
+        return {
+            items: [
+                {
+                    // index: 'businessManagement',
+                    title: '商户配置管理',
+                    nextItems: [
+                        {
+                            index: 'businessList',
+                            title: '商户列表'
+                        },
+                        {
+                            index: 'gameCategory',
+                            title: '游戏分类管理'
+                        },
+                        {
+                            index: 'bannerSetting',
+                            title: 'banner配置'
+                        },
+                        {
+                            index: 'operatelog',
+                            title: '操作记录'
+                        },
+                    ]
+                }
+            ]
+        }
+    },
+};
+</script>
+
+<style>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,112 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+export default new Router({
+    routes: [{
+            path: '/',
+            redirect: '/dashboard'
+        },
+        {
+            path: '/',
+            component: () => import('../components/common/Whole.vue'),
+            meta: {
+                title: '整体页面布局'
+            },
+            children: [
+                {
+                    path: '/dashboard',
+                    component: () => import('../page/Dashboard.vue'),
+                    meta: {
+                        title: '首页'
+                    },
+                    redirect:'/home',     // 该配置是若点击选择父目录时，默认选中该父目录下的子路径页面
+                    children: [
+                        {
+                            path: '/home',
+                            component: () => import('../page/Dashboard.vue'),
+                            meta: {
+                                title: '首页'
+                            },
+                        }               
+                    ]
+                },
+
+
+                {
+                    path: '/business',
+                    component: () => import('../page/business/index.vue'),
+                    meta: {
+                        title: '商户配置管理'
+                    },
+                    redirect:'/business/businessList',
+                    children:[
+                        {
+                            path: 'businessManagement',
+                            meta: {
+                                title: '商户配置管理'
+                            },
+                        },
+                        {
+                            path: 'businessList',
+                            component: () => import('../page/business/businessManagement/businessList.vue'),
+                            meta: {
+                                title: '商户列表'
+                            }
+                        },
+                        {
+                            path: 'gameCategory',
+                            component: () => import('../page/business/businessManagement/gameCategory.vue'),
+                            meta: {
+                                title: '游戏分类管理'
+                            }
+                        },
+                        {
+                            path: 'bannerSetting',
+                            component: () => import('../page/business/businessManagement/bannerSetting.vue'),
+                            meta: {
+                                title: '游戏分类管理'
+                            }
+                        },
+                        {
+                            path: 'operatelog',
+                            component: () => import('../page/business/businessManagement/operatelog.vue'),
+                            meta: {
+                                title: '游戏分类管理'
+                            }
+                        },
+                    ]
+                },
+
+
+                {
+                    path: '/404',
+                    component: () => import('../page/404.vue'),
+                    meta: {
+                        title: '404'
+                    }
+                },
+                {
+                    path: '/403',
+                    component: () => import('../page/403.vue'),
+                    meta: {
+                        title: '403'
+                    }
+                },
+            ]
+        },
+        {
+            // 登录页面
+            path: '/login',
+            component: () => import('../page/Login.vue'),
+            meta: {
+                title: '登录'
+            }
+        },
+        {
+            path: '*',
+            redirect: '/404'
+        }
+    ]
+});
